b3f5589dcd9cfaea14596d1e551e7167c7d2de97
Ghislain MARY <ghislain@ghislainmary.fr>
2026-06-15 23:08:45 +0200

    feat: Add async implementation and update dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,18 @@
 #![no_std]
 
 use crc::{Crc, CRC_8_NRSC_5};
-use embedded_hal::i2c::{Operation, SevenBitAddress};
 #[allow(unused_imports)]
 use micromath::F32Ext;
 
+#[cfg(not(feature = "async"))]
+use embedded_hal as hal;
+#[cfg(feature = "async")]
+use embedded_hal_async as hal;
+
+use hal::i2c::{Operation, SevenBitAddress};
+
 /// The I2C address of the SGP30 chip
-pub const I2C_ADDRESS: SevenBitAddress = 0x58;
+pub const DEFAULT_I2C_ADDRESS: SevenBitAddress = 0x58;
 
 const GET_BASELINE_COMMAND: &[u8] = &[0x20, 0x15];
 const GET_FEATURE_SET_VERSION_COMMAND: &[u8] = &[0x20, 0x2f];
@@ -24,7 +30,7 @@ const SET_HUMIDITY_COMMAND: &[u8] = &[0x20, 0x61];
 #[derive(Debug)]
 pub enum Error<I2cE>
 where
-    I2cE: embedded_hal::i2c::Error,
+    I2cE: hal::i2c::Error,
 {
     /// I²C bus error
     I2c(I2cE),
@@ -42,7 +48,7 @@ where
 
 impl<I2cE> From<I2cE> for Error<I2cE>
 where
-    I2cE: embedded_hal::i2c::Error,
+    I2cE: hal::i2c::Error,
 {
     fn from(value: I2cE) -> Self {
         Error::I2c(value)
@@ -79,9 +85,43 @@ pub struct Sgp30<I2C, D> {
 
 impl<I2C, D> Sgp30<I2C, D>
 where
-    I2C: embedded_hal::i2c::I2c,
-    D: embedded_hal::delay::DelayNs,
+    I2C: hal::i2c::I2c,
+    D: hal::delay::DelayNs,
 {
+    /// Create a new instance of the SGP30 device.
+    #[maybe_async_cfg::maybe(
+        sync(not(feature = "async"), keep_self),
+        async(feature = "async", keep_self)
+    )]
+    pub async fn new(
+        i2c: I2C,
+        address: SevenBitAddress,
+        delay: D,
+    ) -> Result<Self, Error<I2C::Error>> {
+        let mut device = Self {
+            address,
+            delay,
+            i2c,
+            product_version: 0,
+        };
+
+        // Check that the chip is present
+        if device.get_serial_id().await.is_err() {
+            return Err(Error::ChipNotDetected);
+        }
+
+        // Check the feature set
+        let feature_set_version = device.get_feature_set_version().await?;
+        let product_type = (feature_set_version & 0xf000) >> 12;
+        let product_version = (feature_set_version & 0x00ff) as u8;
+        if product_type != 0 || product_version == 0 {
+            return Err(Error::InvalidProduct);
+        }
+        device.product_version = product_version;
+
+        Ok(device)
+    }
+
     /// Get the baseline values for the air quality signals.
     ///
     /// The goal of this feature is to save the baseline at regular intervals
@@ -90,8 +130,12 @@ where
     ///
     /// See [`Sgp30::set_baseline()`] for instructions on how to restore the
     /// values that have been saved here.
-    pub fn get_baseline(&mut self) -> Result<AirQuality, Error<I2C::Error>> {
-        self.get_air_quality(GET_BASELINE_COMMAND, 10)
+    #[maybe_async_cfg::maybe(
+        sync(not(feature = "async"), keep_self),
+        async(feature = "async", keep_self)
+    )]
+    pub async fn get_baseline(&mut self) -> Result<AirQuality, Error<I2C::Error>> {
+        self.get_air_quality(GET_BASELINE_COMMAND, 10).await
     }
 
     /// Start the air quality measurement
@@ -102,15 +146,21 @@ where
     /// compensation algorithm.
     /// This has to be called after every power-up or after each soft reset
     /// performed with [`Sgp30::reset()`].
-    pub fn initialize_air_quality_measure(&mut self) -> Result<(), Error<I2C::Error>> {
-        self.i2c.write(self.address, INIT_AIR_QUALITY_COMMAND)?;
-        self.delay.delay_ms(10);
+    #[maybe_async_cfg::maybe(
+        sync(not(feature = "async"), keep_self),
+        async(feature = "async", keep_self)
+    )]
+    pub async fn initialize_air_quality_measure(&mut self) -> Result<(), Error<I2C::Error>> {
+        self.i2c
+            .write(self.address, INIT_AIR_QUALITY_COMMAND)
+            .await?;
+        self.delay.delay_ms(10).await;
         Ok(())
     }
 
     /// Measure the air quality (CO₂eq and TVOC).
     ///
-    /// This function has to be called at regular invervals of 1s after the air
+    /// This function has to be called at regular intervals of 1s after the air
     /// quality measure has been initialized with
     /// [`Sgp30::initialize_air_quality_measure()`], to ensure the proper
     /// operation of the dynamic baseline compensation algorithm.
@@ -118,8 +168,12 @@ where
     /// For the first 15s after the initialization, the sensor is in an
     /// initialization phase and this function will return an air quality
     /// measure with fixed values of 400 ppm CO₂eq and 0 ppb TVOC.
-    pub fn measure_air_quality(&mut self) -> Result<AirQuality, Error<I2C::Error>> {
-        self.get_air_quality(MEASURE_AIR_QUALITY_COMMAND, 12)
+    #[maybe_async_cfg::maybe(
+        sync(not(feature = "async"), keep_self),
+        async(feature = "async", keep_self)
+    )]
+    pub async fn measure_air_quality(&mut self) -> Result<AirQuality, Error<I2C::Error>> {
+        self.get_air_quality(MEASURE_AIR_QUALITY_COMMAND, 12).await
     }
 
     /// Measure the raw signals (H₂ and ethanol).
@@ -127,12 +181,18 @@ where
     /// <div class="warning">This is intended for part verification and testing
     /// purposes, therefore you should not need it.</div>
     ///
-    /// Itreturns the raw signals of the sensor.
-    pub fn measure_raw_signals(&mut self) -> Result<RawSignals, Error<I2C::Error>> {
-        self.i2c.write(self.address, MEASURE_RAW_SIGNALS_COMMAND)?;
-        self.delay.delay_ms(25);
+    /// It returns the raw signals of the sensor.
+    #[maybe_async_cfg::maybe(
+        sync(not(feature = "async"), keep_self),
+        async(feature = "async", keep_self)
+    )]
+    pub async fn measure_raw_signals(&mut self) -> Result<RawSignals, Error<I2C::Error>> {
+        self.i2c
+            .write(self.address, MEASURE_RAW_SIGNALS_COMMAND)
+            .await?;
+        self.delay.delay_ms(25).await;
         let mut data = [0u8; 6];
-        self.i2c.read(self.address, &mut data)?;
+        self.i2c.read(self.address, &mut data).await?;
         let h2: &[u8; 2] = &data[0..2].try_into().unwrap();
         let h2_crc = data[2];
         let ethanol: &[u8; 2] = &data[3..5].try_into().unwrap();
@@ -145,36 +205,14 @@ where
         })
     }
 
-    /// Create a new instance of the SGP30 device.
-    pub fn new(i2c: I2C, address: SevenBitAddress, delay: D) -> Result<Self, Error<I2C::Error>> {
-        let mut device = Self {
-            address,
-            delay,
-            i2c,
-            product_version: 0,
-        };
-
-        // Check that the chip is present
-        if device.get_serial_id().is_err() {
-            return Err(Error::ChipNotDetected);
-        }
-
-        // Check the feature set
-        let feature_set_version = device.get_feature_set_version()?;
-        let product_type = (feature_set_version & 0xf000) >> 12;
-        let product_version = (feature_set_version & 0x00ff) as u8;
-        if product_type != 0 || product_version == 0 {
-            return Err(Error::InvalidProduct);
-        }
-        device.product_version = product_version;
-
-        Ok(device)
-    }
-
     /// Perform a soft reset.
-    pub fn reset(&mut self) -> Result<(), Error<I2C::Error>> {
-        self.i2c.write(self.address, RESET_COMMAND)?;
-        self.delay.delay_us(600); // Wait for the sensor to enter idle state
+    #[maybe_async_cfg::maybe(
+        sync(not(feature = "async"), keep_self),
+        async(feature = "async", keep_self)
+    )]
+    pub async fn reset(&mut self) -> Result<(), Error<I2C::Error>> {
+        self.i2c.write(self.address, RESET_COMMAND).await?;
+        self.delay.delay_us(600).await; // Wait for the sensor to enter idle state
         Ok(())
     }
 
@@ -187,7 +225,11 @@ where
     /// This needs to be called just after calling
     /// [`Sgp30::initialize_air_quality_measure()`], and prior to any call to
     /// [`Sgp30::measure_air_quality()`].
-    pub fn set_baseline(&mut self, baseline: AirQuality) -> Result<(), Error<I2C::Error>> {
+    #[maybe_async_cfg::maybe(
+        sync(not(feature = "async"), keep_self),
+        async(feature = "async", keep_self)
+    )]
+    pub async fn set_baseline(&mut self, baseline: AirQuality) -> Result<(), Error<I2C::Error>> {
         let mut data = [0u8; 8];
         data[0..2].clone_from_slice(SET_BASELINE_COMMAND);
         let tvoc = Self::get_u8_array_value(baseline.tvoc);
@@ -196,8 +238,8 @@ where
         let co2 = Self::get_u8_array_value(baseline.co2);
         data[5..7].clone_from_slice(&co2);
         data[7] = Self::calc_crc(&co2);
-        self.i2c.write(self.address, &data)?;
-        self.delay.delay_ms(10);
+        self.i2c.write(self.address, &data).await?;
+        self.delay.delay_ms(10).await;
         Ok(())
     }
 
@@ -209,7 +251,11 @@ where
     ///
     /// <div class="warning">This feature may not be available depending on
     /// the version of your sensor.</div>
-    pub fn set_humidity(&mut self, humidity: f32) -> Result<(), Error<I2C::Error>> {
+    #[maybe_async_cfg::maybe(
+        sync(not(feature = "async"), keep_self),
+        async(feature = "async", keep_self)
+    )]
+    pub async fn set_humidity(&mut self, humidity: f32) -> Result<(), Error<I2C::Error>> {
         if self.product_version < 0x20 {
             return Err(Error::FeatureNotSupported);
         }
@@ -221,20 +267,24 @@ where
         ];
         data[2..4].clone_from_slice(&humidity);
         data[4] = Self::calc_crc(&humidity);
-        self.i2c.write(self.address, &data)?;
-        self.delay.delay_ms(10);
+        self.i2c.write(self.address, &data).await?;
+        self.delay.delay_ms(10).await;
         Ok(())
     }
 
-    fn get_air_quality(
+    #[maybe_async_cfg::maybe(
+        sync(not(feature = "async"), keep_self),
+        async(feature = "async", keep_self)
+    )]
+    async fn get_air_quality(
         &mut self,
         command: &[u8],
         wait: u32,
     ) -> Result<AirQuality, Error<I2C::Error>> {
-        self.i2c.write(self.address, command)?;
-        self.delay.delay_ms(wait);
+        self.i2c.write(self.address, command).await?;
+        self.delay.delay_ms(wait).await;
         let mut data = [0u8; 6];
-        self.i2c.read(self.address, &mut data)?;
+        self.i2c.read(self.address, &mut data).await?;
         let co2: &[u8; 2] = &data[0..2].try_into().unwrap();
         let co2_crc = data[2];
         let tvoc = &data[3..5].try_into().unwrap();
@@ -247,24 +297,32 @@ where
         })
     }
 
-    fn get_feature_set_version(&mut self) -> Result<u16, Error<I2C::Error>> {
+    #[maybe_async_cfg::maybe(
+        sync(not(feature = "async"), keep_self),
+        async(feature = "async", keep_self)
+    )]
+    async fn get_feature_set_version(&mut self) -> Result<u16, Error<I2C::Error>> {
         let mut data = [0u8; 3];
         let mut operations = [
             Operation::Write(GET_FEATURE_SET_VERSION_COMMAND),
             Operation::Read(&mut data),
         ];
-        self.i2c.transaction(self.address, &mut operations)?;
+        self.i2c.transaction(self.address, &mut operations).await?;
         let feature_set_version: &[u8; 2] = &data[0..2].try_into().unwrap();
         let feature_set_version_crc = data[2];
         Self::check_crc(feature_set_version, feature_set_version_crc)?;
         Ok(Self::get_u16_value(feature_set_version))
     }
 
-    fn get_serial_id(&mut self) -> Result<u64, Error<I2C::Error>> {
-        self.i2c.write(self.address, GET_SERIAL_ID_COMMAND)?;
-        self.delay.delay_us(500);
+    #[maybe_async_cfg::maybe(
+        sync(not(feature = "async"), keep_self),
+        async(feature = "async", keep_self)
+    )]
+    async fn get_serial_id(&mut self) -> Result<u64, Error<I2C::Error>> {
+        self.i2c.write(self.address, GET_SERIAL_ID_COMMAND).await?;
+        self.delay.delay_us(500).await;
         let mut data = [0u8; 9];
-        self.i2c.read(self.address, &mut data)?;
+        self.i2c.read(self.address, &mut data).await?;
         let id1: &[u8; 2] = &data[0..2].try_into().unwrap();
         let id1_crc = data[2];
         let id2: &[u8; 2] = &data[3..5].try_into().unwrap();
@@ -313,18 +371,21 @@ mod tests {
 
     fn create_device() -> Sgp30<I2cMock, Delay> {
         let expectations = [
-            I2cTransaction::write(I2C_ADDRESS, GET_SERIAL_ID_COMMAND.to_vec()),
+            I2cTransaction::write(DEFAULT_I2C_ADDRESS, GET_SERIAL_ID_COMMAND.to_vec()),
             I2cTransaction::read(
-                I2C_ADDRESS,
+                DEFAULT_I2C_ADDRESS,
                 [0x01, 0x02, 0x17, 0x03, 0x04, 0x68, 0x05, 0x06, 0x50].to_vec(),
             ),
-            I2cTransaction::transaction_start(I2C_ADDRESS),
-            I2cTransaction::write(I2C_ADDRESS, GET_FEATURE_SET_VERSION_COMMAND.to_vec()),
-            I2cTransaction::read(I2C_ADDRESS, [0x00, 0x20, 0x07].to_vec()),
-            I2cTransaction::transaction_end(I2C_ADDRESS),
+            I2cTransaction::transaction_start(DEFAULT_I2C_ADDRESS),
+            I2cTransaction::write(
+                DEFAULT_I2C_ADDRESS,
+                GET_FEATURE_SET_VERSION_COMMAND.to_vec(),
+            ),
+            I2cTransaction::read(DEFAULT_I2C_ADDRESS, [0x00, 0x20, 0x07].to_vec()),
+            I2cTransaction::transaction_end(DEFAULT_I2C_ADDRESS),
         ];
         let i2c = I2cMock::new(&expectations);
-        let mut device = Sgp30::new(i2c, I2C_ADDRESS, Delay {}).unwrap();
+        let mut device = Sgp30::new(i2c, DEFAULT_I2C_ADDRESS, Delay {}).unwrap();
         device.i2c.done();
         device
     }
@@ -332,8 +393,11 @@ mod tests {
     #[test]
     fn get_baseline() {
         let expectations = [
-            I2cTransaction::write(I2C_ADDRESS, GET_BASELINE_COMMAND.to_vec()),
-            I2cTransaction::read(I2C_ADDRESS, [0x02, 0x76, 0x06, 0x02, 0xdd, 0x10].to_vec()),
+            I2cTransaction::write(DEFAULT_I2C_ADDRESS, GET_BASELINE_COMMAND.to_vec()),
+            I2cTransaction::read(
+                DEFAULT_I2C_ADDRESS,
+                [0x02, 0x76, 0x06, 0x02, 0xdd, 0x10].to_vec(),
+            ),
         ];
         let mut device = create_device();
         device.i2c.update_expectations(&expectations);
@@ -344,7 +408,7 @@ mod tests {
     #[test]
     fn initialize_air_quality_measure() {
         let expectations = [I2cTransaction::write(
-            I2C_ADDRESS,
+            DEFAULT_I2C_ADDRESS,
             INIT_AIR_QUALITY_COMMAND.to_vec(),
         )];
         let mut device = create_device();
@@ -356,8 +420,11 @@ mod tests {
     #[test]
     fn measure_air_quality() {
         let expectations = [
-            I2cTransaction::write(I2C_ADDRESS, MEASURE_AIR_QUALITY_COMMAND.to_vec()),
-            I2cTransaction::read(I2C_ADDRESS, [0x02, 0x76, 0x06, 0x02, 0xdd, 0x10].to_vec()),
+            I2cTransaction::write(DEFAULT_I2C_ADDRESS, MEASURE_AIR_QUALITY_COMMAND.to_vec()),
+            I2cTransaction::read(
+                DEFAULT_I2C_ADDRESS,
+                [0x02, 0x76, 0x06, 0x02, 0xdd, 0x10].to_vec(),
+            ),
         ];
         let mut device = create_device();
         device.i2c.update_expectations(&expectations);
@@ -368,8 +435,11 @@ mod tests {
     #[test]
     fn measure_raw_signals() {
         let expectations = [
-            I2cTransaction::write(I2C_ADDRESS, MEASURE_RAW_SIGNALS_COMMAND.to_vec()),
-            I2cTransaction::read(I2C_ADDRESS, [0x00, 0x24, 0xc3, 0x01, 0x51, 0x3a].to_vec()),
+            I2cTransaction::write(DEFAULT_I2C_ADDRESS, MEASURE_RAW_SIGNALS_COMMAND.to_vec()),
+            I2cTransaction::read(
+                DEFAULT_I2C_ADDRESS,
+                [0x00, 0x24, 0xc3, 0x01, 0x51, 0x3a].to_vec(),
+            ),
         ];
         let mut device = create_device();
         device.i2c.update_expectations(&expectations);
@@ -379,7 +449,10 @@ mod tests {
 
     #[test]
     fn reset() {
-        let expectations = [I2cTransaction::write(I2C_ADDRESS, RESET_COMMAND.to_vec())];
+        let expectations = [I2cTransaction::write(
+            DEFAULT_I2C_ADDRESS,
+            RESET_COMMAND.to_vec(),
+        )];
         let mut device = create_device();
         device.i2c.update_expectations(&expectations);
         device.reset().unwrap();
@@ -393,7 +466,7 @@ mod tests {
             tvoc: 733,
         };
         let expectations = [I2cTransaction::write(
-            I2C_ADDRESS,
+            DEFAULT_I2C_ADDRESS,
             [
                 SET_BASELINE_COMMAND[0],
                 SET_BASELINE_COMMAND[1],
@@ -415,7 +488,7 @@ mod tests {
     #[test]
     fn set_humidity() {
         let expectations = [I2cTransaction::write(
-            I2C_ADDRESS,
+            DEFAULT_I2C_ADDRESS,
             [
                 SET_HUMIDITY_COMMAND[0],
                 SET_HUMIDITY_COMMAND[1],
